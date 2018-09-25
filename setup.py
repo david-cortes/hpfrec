ea0f56d7f4c091617a91bbf9777f4403cb054035
David <david.cortes.rivera@gmail.com>
2018-09-25 19:02:31 +0200

    add support for compiling with mingw in windows

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
-import platform
 
-if platform.system() == "Windows":
-	ext_mod = Extension("hpfrec.cython_loops",
-                             sources=["hpfrec/cython_loops.pyx"],
-                             include_dirs=[numpy.get_include()],
-                             extra_compile_args=['/openmp', '/O2'])
-else:
-	ext_mod = Extension("hpfrec.cython_loops",
-                             sources=["hpfrec/cython_loops.pyx"],
-                             include_dirs=[numpy.get_include()],
-                             extra_compile_args=['-fopenmp', '-O3'],
-                             extra_link_args=['-fopenmp'])
-### Comment: -Ofast gives worse speed than -O2 or -O3
+## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
+class build_ext_subclass( build_ext ):
+	def build_extensions(self):
+		c = self.compiler.compiler_type
+		# TODO: add entries for intel's ICC
+		if c == 'msvc': # visual studio
+			for e in self.extensions:
+				e.extra_compile_args = ['/openmp', '/O2']
+		else: # gcc and clang
+			for e in self.extensions:
+				e.extra_compile_args = ['-fopenmp', '-O3']
+				e.extra_link_args = ['-fopenmp']
+				### Comment: -Ofast gives worse speed than -O2 or -O3
+		build_ext.build_extensions(self)
 
 setup(
   name = 'hpfrec',
@@ -27,7 +28,7 @@ setup(
    'scipy',
    'cython'
 ],
-  version = '0.2.2.3',
+  version = '0.2.2.4',
   description = 'Hierarchical Poisson matrix factorization for recommender systems',
   author = 'David Cortes',
   author_email = 'david.cortes.rivera@gmail.com',
@@ -36,7 +37,6 @@ setup(
   keywords = ['poisson', 'probabilistic', 'non-negative', 'factorization', 'variational inference', 'collaborative filtering'],
   classifiers = [],
 
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = [ext_mod,
-    ]
+  cmdclass = {'build_ext': build_ext_subclass},
+  ext_modules = [Extension("hpfrec.cython_loops", sources=["hpfrec/cython_loops.pyx"], include_dirs=[numpy.get_include()])]
 )
