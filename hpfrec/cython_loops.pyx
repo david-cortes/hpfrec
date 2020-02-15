3286737a9a1655aa4ecdeb9d199d6bf6569c909a
david-cortes <david.cortes.rivera@gmail.com>
2020-02-15 15:20:53 +0200

    use 64-bit indexers also in windows

--- hpfrec/cython_loops.pyx
+++ hpfrec/cython_loops.pyx
@@ -12,8 +12,8 @@ import ctypes
 ## parallel for loops with unsigend iterators. If you are using a different compiler, this part can be safely removed
 ## See also: https://github.com/cython/cython/issues/3136
 IF UNAME_SYSNAME == "Windows":
-	obj_ind_type = ctypes.c_long
-	ctypedef long ind_type
+	obj_ind_type = ctypes.c_longlong
+	ctypedef long long ind_type
 	ctypedef double long_double_type
 	obj_long_double_type = ctypes.c_double
 ELSE:
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
 	 'scipy',
 	 'cython'
 ],
-	version = '0.2.2.20',
+	version = '0.2.2.21',
 	description = 'Hierarchical Poisson matrix factorization for recommender systems',
 	author = 'David Cortes',
 	author_email = 'david.cortes.rivera@gmail.com',
